f90b59d016d5c959441cfd426a5c272a3c12f313
Kent Roth <kentencroth@yahoo.com>
2019-06-23 09:48:55 -0500

    WIP: Working on section 4

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,58 @@
-const getNotes = require('./notes');
+const fs = require('fs');
 const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes');
 
-const msg = getNotes();
-console.log(msg);
-console.log(chalk.black.bgBlue('Success!'));
+yargs.version('1.1.0');
+
+// Add Command
+yargs.command({
+	command: 'add',
+	describe: 'Add a new note',
+	builder: {
+		title: {
+			describe: 'Note Title',
+			demandOption: true,
+			type: 'string'
+		},
+		body: {
+			describe: 'Body of note',
+			demandOption: true,
+			type: 'string'
+		}
+	},
+	handler: function(argv) {
+		console.log('Title: ' + argv.title);
+		console.log('Body: ' + argv.body);
+	}
+});
+
+// Remove Command
+yargs.command({
+	command: 'remove',
+	describe: 'Remove a note',
+	builder: {},
+	handler: function() {
+		console.log('Removing Note');
+	}
+});
+
+// List Command
+yargs.command({
+	command: 'list',
+	describe: 'List your notes',
+	handler: function() {
+		console.log('Your notes list');
+	}
+});
+
+// Read Command
+yargs.command({
+	command: 'read',
+	describe: 'Read your notes',
+	handler: function() {
+		console.log('Incoming Note');
+	}
+});
+
+yargs.parse();
